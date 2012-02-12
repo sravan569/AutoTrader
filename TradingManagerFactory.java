d2b72b784f8e5fa996152ef76e144261218f12e1
sravan <sravan_569@yahoo.com>
2012-02-11 19:19:56 -0500

    classesFEB-11-2012

---
+++ TradingManagerFactory.java
@@ -0,0 +1,23 @@
+package Autotraders;
+
+public class TradingManagerFactory {
+TradingManagerFactory(){}
+	//policy selection of trading factory
+	public Object policySelect(int policy_Default)	
+	{
+		if(policy_Default==0){
+			System.out.println("TradingManager object");
+			TradingManager tmObj=new TradingManager();	
+		    return tmObj;
+		}
+		else {
+			System.out.println("TradingManager1 object");
+			TradingManager1 tmObj=new TradingManager1();
+			return tmObj;
+		}
+		
+		
+	}	
+
+	
+}
